4dafac2b3b961e2fe80a89ec42ab936d7670ad52
kshitijaucharmal <kshitijaucharmal21@gmail.com>
2023-06-05 18:20:08 +0530

    Simple program to get seat no of all students

---
+++ __main__.py
@@ -0,0 +1,25 @@
+from pprint import pprint
+import re
+import pdftotext
+import pandas as pd
+
+
+seat_nos = []
+
+# special case page 20
+
+with open('students.pdf', 'rb') as f:
+    content = list(pdftotext.PDF(f))
+    content = content[:-1]
+
+    # Special Case
+    # print(content[19])
+
+    for i,page in enumerate(content):
+        page = list(page.split('\n'))
+        
+        # Get seat_no on line 8(7)
+        seat_no = re.search('F\S*', page[7])[0]
+        seat_nos.append(seat_no)
+
+pprint(seat_nos)
