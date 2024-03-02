c80a3d5a877dfbe6791d38b4a011988ffd00575d
Kshitij Aucharmal <kshitijaucharmal21@gmail.com>
2024-03-02 12:30:00 +0530

    Removing 0 left and integration with UI

--- FE_PBL_using_Pandas.py
+++ FE_PBL_using_Pandas.py
@@ -1,28 +1,29 @@
 # Import the required Modules
 import pandas as pd
 import pdfplumber
-import csv
 from tqdm import tqdm
 
 input_pdf = "big_input.pdf"
+output_csv = 'output.csv'
+
+final_df = pd.DataFrame({
+    "Rollno" : [0],
+    "EM_ISE" : [0], "EM_ESE" : [0], "EM_THEORY_TOT" : [0], "EM_TW" : [0],
+    "SME_ISE" : [0], "SME_ESE" : [0], "SME_THEORY_TOT" : [0], "SME_TW" : [0],
+    "BEE_ISE" : [0], "BEE_ESE" : [0], "BEE_THEORY_TOT" : [0], "BEE_TW" : [0],
+    "EM1_ISE" : [0], "EM1_ESE" : [0], "EM1_THEORY_TOT" : [0], "EM1_TW" : [0],
+    "EP_ISE" : [0], "EP_ESE" : [0], "EP_THEORY_TOT" : [0], "EP_TW" : [0],
+    "BXE_ISE" : [0], "BXE_ESE" : [0], "BXE_THEORY_TOT" : [0], "BXE_TW" : [0],
+    "EC_ISE" : [0], "EC_ESE" : [0], "EC_THEORY_TOT" : [0], "EC_TW" : [0],
+    "PPS_ISE" : [0], "PPS_ESE" : [0], "PPS_THEORY_TOT" : [0], "PPS_TW" : [0],
+    "SGPA" : [0]
+})
 
 def step(df):
+    global final_df
 # Creating a DataFrame with one row of zeros for each column
-    df1 = pd.DataFrame({
-        "Rollno" : [0],
-        "EM_ISE" : [0], "EM_ESE" : [0], "EM_THEORY_TOT" : [0], "EM_TW" : [0],
-        "SME_ISE" : [0], "SME_ESE" : [0], "SME_THEORY_TOT" : [0], "SME_TW" : [0],
-        "BEE_ISE" : [0], "BEE_ESE" : [0], "BEE_THEORY_TOT" : [0], "BEE_TW" : [0],
-        "EM1_ISE" : [0], "EM1_ESE" : [0], "EM1_THEORY_TOT" : [0], "EM1_TW" : [0],
-        "EP_ISE" : [0], "EP_ESE" : [0], "EP_THEORY_TOT" : [0], "EP_TW" : [0],
-        "BXE_ISE" : [0], "BXE_ESE" : [0], "BXE_THEORY_TOT" : [0], "BXE_TW" : [0],
-        "EC_ISE" : [0], "EC_ESE" : [0], "EC_THEORY_TOT" : [0], "EC_TW" : [0],
-        "PPS_ISE" : [0], "PPS_ESE" : [0], "PPS_THEORY_TOT" : [0], "PPS_TW" : [0],
-        "SGPA" : [0]
-    })
-
-# for page 1
-# identifying the course
+    # for page 1
+    # identifying the course
     course_1 = df.iloc[11,0].split(' ')[0]
     course_2 = df.iloc[27,0].split(' ')[0]
 
@@ -57,7 +58,7 @@ def step(df):
         "PPS_ISE" :[0], "PPS_ESE" : [0], "PPS_THEORY_TOT" : [0], "PPS_TW" : [0],
 
         "SGPA" : full_list[11][4] })
-        df1 = pd.DataFrame(pd.concat([df1,new],ignore_index = False))
+        final_df = pd.DataFrame(pd.concat([final_df,new],ignore_index = False))
 
     if(course_1 == "102003-1"):
         # index for separating the marks of subjects 
@@ -80,10 +81,9 @@ def step(df):
         "PPS_ISE" :full_list[9][2], "PPS_ESE" : full_list[9][4], "PPS_THEORY_TOT" : full_list[9][9],
         "PPS_TW" : full_list[10][4],
         "SGPA" : full_list[11][4] })
-        df1 = pd.DataFrame(pd.concat([df1,new], ignore_index =  False))
-            
+        final_df = pd.DataFrame(pd.concat([final_df,new], ignore_index =  False))
         
-# code for result 2
+    # code for result 2
     if(course_2 == "101011-1"):
         # index for separating the marks of subjects
         full_list = splitter([27, 25, 28, 29, 30, 31, 32, 33, 34, 35, 36, 39])
@@ -107,7 +107,7 @@ def step(df):
         "PPS_ISE" :[0], "PPS_ESE" : [0], "PPS_THEORY_TOT" : [0], "PPS_TW" : [0],
 
         "SGPA" : full_list[11][4] })
-        df1 = pd.DataFrame(pd.concat([df1,new],ignore_index = False))
+        final_df = pd.DataFrame(pd.concat([final_df,new],ignore_index = False))
 
         
     if(course_2 == "102003-1"):
@@ -130,25 +130,21 @@ def step(df):
         "PPS_ISE" :full_list[9][2], "PPS_ESE" : full_list[9][4], "PPS_THEORY_TOT" : full_list[9][9],
         "PPS_TW" : full_list[10][4],
         "SGPA" : full_list[11][4] })
-        df1 = pd.DataFrame(pd.concat([df1,new],ignore_index = False))
+        final_df = pd.DataFrame(pd.concat([final_df,new],ignore_index = False))
         
     else:
         print("Course not found")
 
-    df1.to_csv("final.csv", mode='a', index = False)
-    
-# remove line of zeros 
-# df1.drop(2)
-
 # converting pdf to csv
 def pdf_to_csv(input_pdf):
     with pdfplumber.open(input_pdf) as pdf:
-        for i, page in enumerate(tqdm(pdf.pages)):
+        for page in tqdm(pdf.pages):
             file = []
             lines = page.extract_text().split('\n')
             for line in lines[1:]:
                 file.append(line)
             df = pd.DataFrame(file)
             step(df)
-
+    # Write final file
+    final_df.to_csv(output_csv, index = False)
 pdf_to_csv(input_pdf)
